344939948ad5b66038e36e4fadb28304836da365
Bektur Soltobaev <b.soltobaev@disoft.dev>
2021-08-09 13:35:26 +0600

    Add owner field for task

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class Task(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', null=True)
     body = models.TextField()
     is_finished = models.BooleanField(default=False)
     estimated_finish_time = models.DateTimeField()
