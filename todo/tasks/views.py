d9d899c3af919c0bce7ff57ff2112f26c9c74ba4
Bektur Soltobaev <b.soltobaev@disoft.dev>
2021-08-12 22:43:58 +0600

    Add list view

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -2,35 +2,24 @@ from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import views, generics
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
 
-class TaskAPIView(APIView):
+class TaskAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated, ]
-
-    def get(self, request):
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = TaskSerializer(data=request.data)
-
-        if serializer.is_valid():
-            owner = request.user
-            task = Task.objects.create(
-                owner=owner,
-                body=serializer.validated_data.get('body'),
-                estimated_finish_time=serializer.validated_data.get('estimated_finish_time')
-            )
-            serializer = TaskSerializer(instance=task)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 
-class TaskDetailAPIView(APIView):
+class TaskCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated, ]
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDetailAPIView(views.APIView):
     permission_classes = [IsAuthenticated, ]
 
     def get(self, request, id):
@@ -53,7 +42,7 @@ class TaskDetailAPIView(APIView):
         return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class SetFinishedTaskAPIView(APIView):
+class SetFinishedTaskAPIView(views.APIView):
 
     def post(self, request, id):
         task = get_object_or_404(Task, id=id)
